a6dff9210c3b2355471f2fc8b9950ad1feac1221
Diego Lora <diego.lora@usil.pe>
2020-04-28 00:16:03 -0500

    Start

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="author" content="GRUPO ">
+	<meta name="description" content="Herramienta de creación de base de datos">
+	<meta name="keywords" content="DB, tool, SQL">
+	<title>DB Simple</title>
+
+	<!-- styles -->
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+
+	<!-- fonts -->
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"> <!--Roboto-->
+</head>
+
+<body>
+
+</body>
+
+<footer>
+	
+</footer>
+
+</html>
---
+++ sql/generate-IE.sql
@@ -0,0 +1,9 @@
+/*Institucion educativa*/
+
+CREATE TABLE Alumnos (
+	id_Alumno INT NOT NULL,
+	nombre VARCHAR(100) NOT NULL,
+	apellido VARCHAR(100) NOT NULL,
+	dni CHAR(8) NOT NULL,
+	telefono CHAR(8)
+)
