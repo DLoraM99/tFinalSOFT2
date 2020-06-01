3f50a617dd9f10fdd6c16d5ff3836113a23b7efd
DLoraM99 <57108078+DLoraM99@users.noreply.github.com>
2020-06-01 16:18:06 -0500

    Delete generate-IE.sql

--- sql/generate-IE.sql
+++
@@ -1,9 +0,0 @@
-/*Institucion educativa*/
-
-CREATE TABLE Alumnos (
-	id_Alumno INT NOT NULL,
-	nombre VARCHAR(100) NOT NULL,
-	apellido VARCHAR(100) NOT NULL,
-	dni CHAR(8) NOT NULL,
-	telefono CHAR(8)
-)
