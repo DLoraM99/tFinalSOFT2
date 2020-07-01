dac3fb794887fd599584fde93dedc49fd4d993cd
DLoraM99 <57108078+DLoraM99@users.noreply.github.com>
2020-06-30 23:32:46 -0500

    Add files via upload

---
+++ connection/connection.php
@@ -0,0 +1,27 @@
+
+<?php
+
+class Conexion {
+            
+    private static $instancia = null;
+    public function __construct()
+    {
+        try {
+            self::$instancia = new PDO('mysql:host=localhost;dbname=dbsimple','root', '');
+            self::$instancia->exec("SET CHARACTER SET utf8");
+        } catch (PDOException $e) {
+            print "Error!: " . $e->getMessage();
+            die();
+        }           
+    }
+    public static function singleton()
+    {
+        if (!isset(self::$instancia)) {
+            $miclase = __CLASS__;
+            self::$instancia = new $miclase;
+        }
+        return self::$instancia;
+    }
+}
+
+?>
